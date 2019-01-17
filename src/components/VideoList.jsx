424231c3c83ae4b2c933b8954af671e3fe03f56f
gakittle <greg.kittleson@gmail.com>
2019-01-16 16:59:30 -0800

    Refactor VideoList to dyncamically call VideoListEntry

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,16 @@
+import exampleVideoData from '/src/data/exampleVideoData.js';
+import VideoListEntry from './VideoListEntry.js';
+
 var VideoList = () => (
   <div className="video-list">
+    {exampleVideoData.map(video =>
+      <VideoListEntry video={video}/>
+    )}
+    {/* <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div><h5><em>videoListEntry</em> view goes here</h5></div> */}
   </div>
 );
 
