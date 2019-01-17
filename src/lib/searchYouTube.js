ad800bee44a015e943667aae5f116830428684a2
gakittle <greg.kittleson@gmail.com>
2019-01-16 21:10:54 -0800

    Partially implement YouTube API search request

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,6 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  return ($.get('https://www.googleapis.com/youtube/v3/search', {q: options.query, maxResults: options.max, key: options.key, part: 'snippet', videoEmbeddable: 'true'})
+  );
 };
 
 export default searchYouTube;
