25b8d9ebf57ebe7810cb847c22baca3cbc4327bb
gakittle <greg.kittleson@gmail.com>
2019-01-16 19:10:49 -0800

    Implement video player with default video

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,14 +1,35 @@
-var VideoPlayer = () => (
-  <div className="video-player">
-    <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
-    </div>
-    <div className="video-player-details">
-      <h3>Video Title</h3>
-      <div>Video Description</div>
-    </div>
-  </div>
-);
+import exampleVideoData from '/src/data/exampleVideoData.js';
+
+var VideoPlayer = (props) => {
+  var domain = 'https://www.youtube.com/embed/';
+  var vidId = props.video.id.videoId;
+  
+  if (props) {
+    return (
+      <div className="video-player">
+        <div className="embed-responsive embed-responsive-16by9">
+          <iframe className="embed-responsive-item" src={domain + vidId} allowFullScreen></iframe>
+        </div>
+        <div className="video-player-details">
+          <h3>{props.video.snippet.title}</h3>
+          <div>{props.video.snippet.description}</div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="video-player">
+        <div className="embed-responsive embed-responsive-16by9">
+          <iframe className="embed-responsive-item" src={domain + exampleVideoData[0].id.videoId} allowFullScreen></iframe>
+        </div>
+        <div className="video-player-details">
+          <h3>{exampleVideoData[0].snippet.title}</h3>
+          <div>{exampleVideoData[0].snippet.description}</div>
+        </div>
+      </div>
+    );
+  }
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
