26ff7ea17295ffeedf18bd3683f0bcd470b7b96e
gakittle <greg.kittleson@gmail.com>
2019-01-17 10:40:21 -0800

    Implement searchYouTube function with passing GET request

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,14 @@
 var searchYouTube = (options, callback) => {
-  return ($.get('https://www.googleapis.com/youtube/v3/search', {q: options.query, maxResults: options.max, key: options.key, part: 'snippet', videoEmbeddable: 'true'})
+  return ($.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: {q: options.query, maxResults: options.max, key: options.key, part: 'snippet', type: 'video', videoEmbeddable: 'true'},
+    type: 'GET',
+    contentType: 'application/json',
+    success: (data) => {
+      callback(data.items);
+    },
+    error: (error) => { console.log('error: unsuccessful search', error); }
+  })
   );
 };
 
